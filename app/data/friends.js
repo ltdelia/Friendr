789a822075be3d0e0dad176f48f450401f6202e8
ltdelia <delia.louis@gmail.com>
2016-04-26 22:05:54 -0400

    added dropdowns

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,9 +3,9 @@ var allMyFriends = [
 	{
 		name: "Hans",
 		photo: "https://pbs.twimg.com/profile_images/2468030199/nicemoleman_400x400.jpg",
-		// scores: []
+		scores: [4, 2, 1, 1, 5, 3, 3, 2, 5, 3]
 	}
 
-
-
 ]
+
+module.exports = allMyFriends;
