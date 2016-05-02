e20461cbf1b77212ed4b386d914fa634f3f6fa26
ltdelia <delia.louis@gmail.com>
2016-05-02 13:26:31 -0400

    revised compatibility logic for compounding error

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,10 +11,10 @@ module.exports = function(app){
 		// push the submitted data to the allMyFriends array
 		allMyFriends.push(req.body);
 
+		var diffs = [];
+		var diffSum = 0;
 		// loop through the allMyFriends array of friends
 		for(var i=0; i<allMyFriends.length-1; i++){
-			var diffs = [];
-			var diffSum = 0;
 			// loop through the scores of that friend
 			for(var j=0; j<allMyFriends[i].scores.length; j++){
 				if(allMyFriends[i] == undefined){
@@ -22,17 +22,20 @@ module.exports = function(app){
 				}else{
 					// calculate the difference in scores
 					var difference = Math.abs(allMyFriends[i].scores[j] - allMyFriends[i+1].scores[j]);	
+					console.log("The difference between "+allMyFriends[i].scores[j]+" and"+allMyFriends[i+1].scores[j]+" is "+difference);
 					diffs.push(difference);
-
-					// add up the difference
-					for(var k=0; k<diffs.length; k++){
-						diffSum+=diffs[k];
-						console.log(diffSum);
-					}
-
 				}
 			}
+			// add up the difference
+			for(var k=0; k<diffs.length; k++){
+				diffSum+=diffs[k];
+				console.log(diffSum);
+			}
 		}
+
+		// current issues
+		// diffs array is not emptying after a second person is added
+		// having trouble handling logic when a third or more people are added 
 
 		console.log(diffs);
 
